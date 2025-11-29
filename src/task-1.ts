485765a552e9a92c3b5695a58e134b397f85663d
Kira Yukhno <kira.yukhno123@gmail.com>
2025-11-29 20:54:33 +0200

    fix the first task

--- src/task-1.ts
+++ src/task-1.ts
@@ -1,9 +1,7 @@
-const user: { name: string; age: number;  isOnline: boolean} = {
-    name: "Alice",
-    age: 30,
-    isOnline: true
-};
+const name: string = "Alice";
+const age: number = 30;
+const isOnline: boolean = true;
 
-console.log(`Name: ${user.name}`);
-console.log(`Age: ${user.age}`);
-console.log(`Online: ${user.isOnline}`);
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Online: ${isOnline}`);
